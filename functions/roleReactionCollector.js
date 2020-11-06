58a331d27d2298e0d3ff6a5bb70b2c83b7ba0d37
jpmsen <52090304+jpmsen@users.noreply.github.com>
2020-11-06 19:43:18 +0100

    fix non-cached users not being able to be assigned a role

--- functions/roleReactionCollector.js
+++ functions/roleReactionCollector.js
@@ -2,25 +2,25 @@ const Keyv = require('keyv');
 const { version, database } = require('../config.json');
 const keyvRoles = new Keyv(`${database.type}://${database.user}${database.password === "" ? '' : ':'}${database.password}@${database.connection}:${database.port}/${database.name}`, { namespace: 'roles' });
 const keyvRoleEmbedMessages = new Keyv(`${database.type}://${database.user}${database.password === "" ? '' : ':'}${database.password}@${database.connection}:${database.port}/${database.name}`, { namespace: 'roleEmbedMessages' });
-
+const Discord = require('discord.js');
 const emojis = require('../emojis/unicodeEmojis.js');
+
 module.exports = {
     async setReactionCollector(msg) {
         // Only react when these emojis have been pressed
-        const filter = (reaction) => {
+        const filter = (reaction, user) => {
             return [emojis[1], emojis[2], emojis[3], emojis[4], emojis[5], emojis[6], emojis[7], emojis[8], emojis[9]].includes(reaction.emoji.name);
         };
 
         // create a reaction collector
-        console.log(msg);
         const collector = msg.createReactionCollector(filter, { dispose: true });
 
         // collectors event listeners.
-        collector.on('collect', async(reaction, user) => {
+        collector.on('collect', (reaction, user) => {
             manageUserRole(msg, reaction, user, 'collect');
         });
 
-        collector.on('remove', async(reaction, user) => {
+        collector.on('remove', (reaction, user) => {
             manageUserRole(msg, reaction, user, 'remove');
         });
     },
@@ -30,6 +30,9 @@ module.exports = {
 async function manageUserRole(msg, reaction, user, action) {
     const roleEmbedMessages = await keyvRoleEmbedMessages.get(msg.guild.id);
     const roles = await keyvRoles.get(msg.guild.id);
+
+    // When users are not cached into the guild yet, find them using a fetch statement.
+    await msg.guild.members.fetch(user.id).then(nonCachedUser => user = nonCachedUser);
 
     roleEmbedMessages.forEach((embedMsg, index) => {
         if (embedMsg === msg.id) {
